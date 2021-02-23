eadb3ab8cb9aa696d8de8175d635e9e52c3702e2
nurzhan23 <78630767+nurzhan23@users.noreply.github.com>
2021-02-24 00:48:18 +0600

    Add files via upload

---
+++ lab 4/src/app/products.ts
@@ -0,0 +1,122 @@
+export const products = [
+  {
+    id: 1,
+    name:
+      "New Apple iPhone 12 Pro Max (128GB, Graphite) [Locked] + Carrier Subscription",
+    rating: 5.0,
+    description: "A large phone with one of the best screens",
+    img: "https://m.media-amazon.com/images/I/71XXJC7V8tL._FMwebp__.jpg",
+    url:
+      "https://www.amazon.com/dp/B08L5R1CCC/ref_=vn_s_iwp_0?pd_rd_w=VgnoL&pf_rd_p=84cbcc8f-0fe2-44fc-ae2a-87f8b101dbca&pf_rd_r=2XQ5WXDXBAN72M7SHT8B&pd_rd_r=03b7b8b4-026c-456e-83dc-1613ee32ff32&pd_rd_wg=FYF4s&qid=1614090436"
+  },
+  {
+    id: 2,
+    name: "TCL 43S325 43 Inch 1080p Smart LED Roku TV (2019)",
+    rating: 4.5,
+    description:
+      "Smart Functionality offers access to over 5,000 streaming channels featuring more than 500,000 movies and TV episodes via Roku TV",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71YZlXXFktL._AC_SL1500_.jpg",
+    url:
+      "https://www.amazon.com/TCL-43S325-Inch-1080p-Smart/dp/B07G9W9ZR9/ref=sr_1_1?dchild=1&field-shipping_option-bin=3242350011&pf_rd_i=16225009011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=85a9188d-dbd5-424e-9512-339a1227d37c&pf_rd_r=ZQ53BEFAD2NZB29CZ5FP&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1588978526&rnid=1266092011&s=electronics&sr=1-1"
+  },
+  {
+    id: 3,
+    name:
+      "essence | Lash Princess False Lash Effect Mascara | Gluten & Cruelty Free",
+    rating: 4.8,
+    description:
+      " Lash Princess False Lash Mascara defines and separates lashes while achieving a bold look.",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61nvmVTF12L._SL1500_.jpg",
+    url:
+      "https://www.amazon.com/essence-Princess-Effect-Mascara-Cruelty/dp/B00T0C9XRK/ref=sr_1_1?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=H5HJYQTKV28588M5Y8HK&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-1"
+  },
+  {
+    id: 4,
+    name:
+      "Toshiba (HDTB420XK3AA) Canvio Basics 2TB Portable External Hard Drive USB 3.0, Black",
+    rating: 4.7,
+    description:
+      "Quickly add more storage capacity to your PC and other compatible devices",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/910A6B1Sa4L._AC_SL1500_.jpg",
+    url:
+      "https://www.amazon.com/Toshiba-HDTB420XK3AA-Canvio-Portable-External/dp/B079D3D8NR/ref=sr_1_1_sspa?dchild=1&keywords=hard+drives&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=JGRR6P7KFQD1JEDF3JQZ&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614091007&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNE9UQlhOSzBRQjFOJmVuY3J5cHRlZElkPUEwNDU1NTIwQUwzWEZTVDFROFBXJmVuY3J5cHRlZEFkSWQ9QTAzMTkzOTNKVzhYQTFSOFZZVzgmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"
+  },
+  {
+    id: 5,
+    name:
+      "CeraVe Moisturizing Cream | Body and Face Moisturizer for Dry Skin | Body Cream with Hyaluronic Acid and Ceramides | 19 Ounce",
+    rating: 4.9,
+    description:
+      "With hyaluronic acid, ceramides and MVE technology for 24 hour hydration. Rich, velvety texture that leaves skin feeling smooth, it is absorbed quickly for softened skin without greasy, sticky, feel.",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61S7BrCBj7L._SL1000_.jpg",
+    url:
+      "https://www.amazon.com/CeraVe-Moisturizing-Cream-Daily-Moisturizer/dp/B00TTD9BRC/ref=sr_1_1?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=X5JCBDHZT7MVKJ2DA0BW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-1"
+  },
+  {
+    id: 6,
+    name: "HP 65 | Ink Cartridge | Black | N9K02AN",
+    rating: 4.2,
+    description: "Original HP ink cartridges: Genuine ink for your HP printer.",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/717HLlpdcIL._AC_SL1500_.jpg",
+    url:
+      "https://www.amazon.com/HP-Original-Cartridge-N9K02AN-DeskJet/dp/B01BYKUI9C/ref=sr_1_1?dchild=1&fst=as%3Aoff&pf_rd_i=16225007011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=74069509-93ef-4a3c-8dca-a9e3fa773a64&pf_rd_r=N56DQQF1DW9K9XGEFJ0E&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1487012920&rnid=16225007011&s=computers-intl-ship&sr=1-1"
+  },
+  {
+    id: 7,
+    name:
+      "Amazon Basics 8-Sheet Capacity, Cross-Cut Paper and Credit Card Shredder, 4.1 Gallon",
+    rating: 4.3,
+    description:
+      "Cross-cut shred size: turns paper into small confetti-like pieces measuring 3/16 by 1-27/32 inches (5 by 47 mm); meets security level P-3 standards",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71K7Q4FpguL._AC_SL1500_.jpg",
+    url:
+      "https://www.amazon.com/AmazonBasics-8-Sheet-Cross-Cut-Credit-Shredder/dp/B00YFTHJ9C/ref=sr_1_1?dchild=1&keywords=amazonbasics&pd_rd_r=d33fa053-2a8d-4e50-9e64-6e315fe70336&pd_rd_w=lVpLH&pd_rd_wg=tiZS7&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=RFX5GAX41CQC244HRMM6&qid=1614091296&sr=8-1"
+  },
+  {
+    id: 8,
+    name: "OXO Good Grips Bottle Brush",
+    rating: 4.0,
+    description:
+      "Long, durable neck flexes for easy reach inside bottles, pitchers and carafes",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/41aeQ60Y03L._AC_SL1500_.jpg",
+    url:
+      "https://www.amazon.com/OXO-Good-Grips-Bottle-Brush/dp/B00004OCLJ/ref=sr_1_3?dchild=1&fst=as%3Aoff&pf_rd_i=16225014011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a3460e00-9eac-4cab-9814-093998a3f6d8&pf_rd_r=05TCZF38C4D4PV6EV33A&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1489014608&rnid=16225014011&s=sporting-goods&sr=1-3"
+  },
+  {
+    id: 9,
+    name:
+      "Plush Toys 10 Inch Tall Stuffed Toy for Child，Soft Figure，Carefully Handmade Soft Plush Cotton",
+    rating: 4.1,
+    description:
+      "Material: handmade., made of high-quality plush materials, maximum quality you can sense at first touch",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/31zsyDOiq7L._AC_.jpg",
+    url:
+      "https://www.amazon.com/Stuffed-Child%EF%BC%8CSoft-Figure%EF%BC%8CCarefully-Handmade-Cotton/dp/B08V58NVYV/ref=sr_1_1_sspa?dchild=1&keywords=toy+figures&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=MFS1HHF8P7CSMCZJ06GW&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614091388&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzTzRCR09CTFRXWTZEJmVuY3J5cHRlZElkPUEwNTQxODg0MzRYNFBCRldRNDU4RiZlbmNyeXB0ZWRBZElkPUEwMjk1OTc5SVlNUFo4MU0zTkxDJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 10,
+    name:
+      "Amazon Basics 36 Pack AAA High-Performance Alkaline Batteries, 10-Year Shelf Life, Easy to Open Value Pack",
+    rating: 3.9,
+    description:
+      "AAA 1.5-volt performance alkaline batteries; ideal for a variety of devices, including game controllers, toys, flashlights, digital cameras, and clocks",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71nDX36Y9UL._AC_SL1026_.jpg",
+    url:
+      "https://www.amazon.com/AmazonBasics-Performance-Alkaline-Batteries-Count/dp/B00LH3DMUO/ref=sr_1_4?dchild=1&keywords=amazonbasics&pd_rd_r=fcfdc7f7-4dbe-4ac3-bd75-cfdbf3ee73a7&pd_rd_w=gRmQj&pd_rd_wg=6WGtR&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=98BZK5FZVVPY2KZPA1A3&qid=1614091418&sr=8-4"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
